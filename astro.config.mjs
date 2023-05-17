a9ca2375ce3ae18650fac82aebd5c213e3f0e15f
Fletcher Hardison <fhardison@@gmail.com>
2023-05-17 02:03:41 -0400

    fixing urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-minimal-starter.netlify.app/",
+  site: "https://amindforlanguage.com/",
+  base: "/lgpsi-extras",
   integrations: [sitemap()],
 outDir: 'docs',
 });
